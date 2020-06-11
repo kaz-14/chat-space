67412e7aa8fd70e46cc6932b3f5a01b26c00b90d
kaz-14 <66046956+kaz-14@users.noreply.github.com>
2020-06-11 18:12:58 +0900

    change ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chatkzls.pem'] 
+                  keys: ['~/.ssh/chat-space.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
