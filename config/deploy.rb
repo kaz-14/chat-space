622b277bc2e09ae601341d13fddf60f7d8d4a1af
kaz-14 <66046956+kaz-14@users.noreply.github.com>
2020-06-11 16:46:50 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chatkzls_pem'] 
+                  keys: ['~/.ssh/chatkzls_keys'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
